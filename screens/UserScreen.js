79372cec4213b3c335ac16bc5ea5c9f0473f4fab
David Molins Gracia <dmolins@client231-10-192-64.eduroam.upc.edu>
2019-10-13 02:39:30 +0200

    Versió login en 4a finestra

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,14 +1,118 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TextInput, 
+  TouchableOpacity, AsyncStorage, Keyboard } from 'react-native';
+  import {Actions} from 'react-native-router-flux';
+
+
 
 class UserScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#f5efdf' }}>
-        <Text>USER Screen!</Text>
-      </View>
-    );
-  }
+   constructor(props){
+       super(props);
+       this.state={
+           email:'',
+           password: ''
+       }
+   }
+   saveData =async()=>{
+       const {email,password} = this.state;
+       //save data with asyncstorage
+       let loginDetails={
+           email: email,
+           password: password
+       }
+       if(this.props.type !== 'Login')
+       {
+           AsyncStorage.setItem('loginDetails', JSON.stringify(loginDetails));
+           Keyboard.dismiss();
+           alert("You successfully registered.");
+           this.login();
+       }
+       else if(this.props.type == 'Login')
+       {
+           try{
+               let loginDetails = await AsyncStorage.getItem('loginDetails');
+               let ld = JSON.parse(loginDetails);
+               if (ld.email != null && ld.password != null)
+               {
+                   if (ld.email == email && ld.password == password)
+                   {
+                       alert('Go in!');
+                   }
+                   else
+                   {
+                       alert('Email and Password does not exist!');
+                   }
+               }
+           }catch(error)
+           {
+               alert(error);
+           }
+       }
+   }
+   showData = async()=>{
+       let loginDetails = await AsyncStorage.getItem('loginDetails');
+       let ld = JSON.parse(loginDetails);
+       alert('email: '+ ld.email + ' ' + 'password: ' + ld.password);
+   }
+   render() {
+       return(
+           <View style={styles.container}>
+               <Text style={styles.welcomeText}>Welcome to Kuby!</Text>
+               <TextInput style={styles.inputBox}
+               onChangeText={(email) => this.setState({email})}
+               underlineColorAndroid='rgba(0,0,0,0)'
+               placeholder="Email"
+               placeholderTextColor = "#002f6c"
+               selectionColor="#fff"
+               keyboardType="email-address"
+               onSubmitEditing={()=> this.password.focus()}/>
+               <TextInput style={styles.inputBox}
+               onChangeText={(password) => this.setState({password})}
+               underlineColorAndroid='rgba(0,0,0,0)'
+               placeholder="Password"
+               secureTextEntry={true}
+               placeholderTextColor = "#002f6c"
+               ref={(input) => this.password = input}
+               />
+               <TouchableOpacity style={styles.button}>
+                   <Text style={styles.buttonText} onPress={this.saveData}>Log in{this.props.type}</Text>
+               </TouchableOpacity>
+           </View>
+       )
+   }
 }
+const styles = StyleSheet.create({
+   container: {
+       flex: 1,
+       justifyContent: 'center',
+       alignItems: 'center',
+   },
+   inputBox: {
+       width: 300,
+       backgroundColor: '#eeeeee',
+       borderRadius: 25,
+       paddingHorizontal: 16,
+       fontSize: 16,
+       color: '#002f6c',
+       marginVertical: 10
+   },
+   button: {
+       width: 300,
+       backgroundColor: '#4f83cc',
+       borderRadius: 25,
+       marginVertical: 10,
+       paddingVertical: 12
+   },
+   buttonText: {
+       fontSize: 16,
+       fontWeight: '500',
+       color: '#ffffff',
+       textAlign: 'center'
+   },
+   welcomeText: {
+       fontWeight: 'bold',
+       fontSize: 25,
+   }
+})
 
 export default UserScreen;
